458dbdc29bd4bcfc0583711c2a3cc6b0dd937123
meqbal73 <51688022+meqbal73@users.noreply.github.com>
2026-07-08 21:00:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Project URL: Settings > API > Project URL
 // Publishable key (أو anon key للمشاريع القديمة): Settings > API
 window.MARN_CONFIG = {
-  SUPABASE_URL: 'qkurbzsoenwlwiduoqnr.supabase.co',
+  SUPABASE_URL: 'https://qkurbzsoenwlwiduoqnr.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_EIFdzHi0Q6KMay_wzMcjGw_V2xLx8EZ'
 };
